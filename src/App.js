8d22a0131ddbccf1ada8e6db611ce45b3b574a70
ALIMANDAL4124 <alimandal680@gmail.com>
2025-01-22 04:28:59 +0000

    add

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import TableComponent from './components/TableComponent'
+
+import './App.css'
+
+const App = () => (
+  <div className="app-container">
+    <TableComponent />
+  </div>
+)
+export default App
